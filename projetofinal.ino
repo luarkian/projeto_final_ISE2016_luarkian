eb8441d1acf5a1ebd1058b6af65816be44023e7f
luarkian <kaype_x@hotmail.com>
2017-03-07 13:59:44 -0400

    :)

--- projetofinal.ino
+++ projetofinal.ino
@@ -1,6 +1,6 @@
 // Definição pino do sensor
 int pin_a0 = A0;
-int oxi =250;
+int oxi =100;
 //Definição do pino do motor
 int pin_motor = 7;
 
@@ -16,19 +16,23 @@ void setup() {
 void loop() {
    // Ler os dados do pino analogico A0 do sensor
   int valor_analogico = analogRead(pin_a0);
- 
+ Serial.print(" Pino A0 : ");
+   Serial.println(valor_analogico);
+   
   if(valor_analogico > oxi){
-  
-      //Ligar o a Bomba
-      digitalWrite(pin_motor,HIGH);
-      
-   // Mostra os dados no serial monitor
+     // Mostra os dados no serial monitor
       Serial.print(" Pino A0 : ");
       Serial.println(valor_analogico);
+      
+      Serial.println("MOTOR ATIVO");
+      //Ligar o a Bomba
+      digitalWrite(pin_motor,HIGH);
    
    //Deixa a bomba ligado por 15 minutos   
       delay(900000);    
+   
     //Desligar o motor  
       digitalWrite(pin_motor,LOW);
+       Serial.println("MOTOR DESATIVADO");
   }
 }
